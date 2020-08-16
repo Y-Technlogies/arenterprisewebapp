d433d16a20c5cebfaa5023db1bdc34feaec1a880
Md.Mohib Ullah <moin.uoda@gmail.com>
2020-08-16 21:50:16 +0600

    change layout

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -1,67 +1,99 @@
 @extends('mainlayouts.app')
 
 @section('content')
-    <h1>Create Invoice</h1>
-    {!! Form::open(['action' => 'InvoiceController@exportPdf', 'method' => 'POST', 'enctype' => 'multipart/form-data'])!!}
-    <div class="form-row">
-        <div class="form-group col-md-3">
-            {{Form::label('invoiceno', 'Invoice number')}}
-            {{Form::text('agents_sn', '', ['class' => 'form-control', 'placeholder' => 'Invoice number'])}}
-        </div>
-        <div class="form-group col-md-3">
-            {{Form::label('invoicetitle', 'Invoice Title')}}
-            {{Form::text('invoicetitle', '', ['class' => 'form-control', 'placeholder'=>'Title'])}}
-        </div>
-        <div class="form-group col-md-3" >
-            {{Form::label('clientname', 'Client Serial')}}
-            {{Form::text('clientname', '', ['class' => 'form-control', 'placeholder' => 'Client Name'])}}
-        </div>
-       <div class="form-group col-md-3" >
-            {{Form::label('clientname', 'Client Name')}}
-            {{Form::text('clientname', '', ['class' => 'form-control', 'placeholder' => 'Client Name'])}}
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="form-group col-md-3">
-            {{Form::label('clientaddress', 'Client Address')}}
-            {{Form::textarea('clientaddress', '', ['class' => 'form-control', 'placeholder' => 'Client Address'])}}
-        </div>
-        <div class="form-group col-md-3">
-            {{Form::label('agent_name', 'Agent Name')}}
-            <select name="agent_name" class="form-control">
-                <option value="0">Select Agent</option>
-                @foreach($agents as $agent)
-                    <option value="{{ $agent->agent_id }}">{{ $agent->full_name() }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group col-md-3">
-            {{Form::label('clientaddress', 'Destination')}}
-            {{Form::textarea('clientaddress', '', ['class' => 'form-control', 'placeholder' => 'Client Address'])}}
-        </div>
-        <div class="form-group col-md-3">
-            {{Form::label('salesdivision', 'Sales Division')}}
-            {{Form::text('salesdivision', '', ['class' => 'form-control', 'placeholder' => 'Sales Division'])}}
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="form-group col-md-3">
-            {{Form::label('invoicedate', 'Invoice Date')}}
-            {{Form::text('invoicedate', '', ['class' => 'form-control', 'placeholder' => 'Invoice Date'])}}
-        </div>
-        <div class="form-group col-md-3">
-            {{Form::label('duedate', 'Due Date')}}
-            {{Form::text('duedate', '', ['class' => 'form-control', 'placeholder' => 'Due Date'])}}
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="col-md-12">
-            <invoice-table></invoice-table>
-        </div>
-    </div>
-    <div class="form-group col-md-3 pl-0">
-    {{Form::submit('Submit', ['class'=>'btn btn-dark'])}}
-    {!! Form::close() !!}
-    </div>
+        <h1>Create Invoice</h1>
+        <table class="table">
+            <tbody>
+            <tr>
+                <td rowspan="9" style="border: 0;"></td>
+                <td colspan="3" rowspan="4" width="50%">
+                    A.R Enterprice <br>
+                    191 K Y Plaza(4th floor) <br>
+                    Fokira Pool, Motijheel C/A <br>
+                    Dhaka-1000 <br>
+                    Phone:02-7191365 <br>
+                    Mobile: 01716-511288, 01705-478851 <br>
+                    Email: arenterprisebd2012@gmail.com, powerlube12@gmail.com
+
+                </td>
+                <td colspan="2">
+                    Invoice No <br>
+                    <b>
+                        2010
+                    </b>
+                </td>
+                <td colspan="2">
+                    Date <br>
+                    <b>
+                        12-Mar-2021
+                    </b>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    Delivery Note: <br>
+
+                </td>
+                <td colspan="2">Mode/Term of payment: <br>
+                    <b>
+                        Cradit
+                    </b>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    Aggent: <br>
+                    <b>Md. Main Uddin</b>
+                </td>
+                <td colspan="2">Client <br></td>
+            </tr>
+            <tr>
+                <td colspan="2">Buyer's order No: <br></td>
+                <td colspan="2">Date <br></td>
+            </tr>
+            <tr>
+                <td colspan="3" rowspan="3">Buyer: <br></td>
+                <td colspan="2">Dispatch Document No: <br></td>
+                <td colspan="2">Date</td>
+            </tr>
+            <tr>
+                <td colspan="2">Address <br></td>
+                <td colspan="2">Destination <br></td>
+            </tr>
+            <tr>
+                <td colspan="4" rowspan="2">Terms of Delivery: <br></td>
+            </tr>
+            <tr>
+                <td colspan="3">28<br></td>
+            </tr>
+            <tr>
+                <td colspan="3" class="text-center">Description of Gods</td>
+                <td>Quantity/Cat</td>
+                <td>Rate/Cat</td>
+                <td colspan="2">Amount</td>
+            </tr>
+            <tr>
+                <td style="border: 0; width: 1%">
+                    <a href="#" class="btn btn-info"><i class="fa fa-plus"></i></a>
+                </td>
+                <td colspan="3">20</td>
+                <td>21</td>
+                <td>22</td>
+                <td colspan="2">23</td>
+            </tr>
+            <tr>
+                <td style="border: 0;"></td>
+                <td colspan="3" class="text-right">Total</td>
+                <td>25</td>
+                <td>26</td>
+                <td colspan="2">27</td>
+            </tr>
+            </tbody>
+        </table>
 @endsection
 
+<style>
+    td {
+        border : 1px solid #dee2e6;
+    }
+</style>
