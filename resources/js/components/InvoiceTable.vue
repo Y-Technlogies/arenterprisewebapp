e0a1792319d2fab6d23002fadb66df2c9cc3aaa9
Md.Mohib Ullah <moin.uoda@gmail.com>
2020-08-17 16:43:35 +0600

    invoice part 2

--- resources/js/components/InvoiceTable.vue
+++ resources/js/components/InvoiceTable.vue
@@ -1,49 +1,103 @@
 <template>
-    <div>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Product Name</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Rate</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(product, index) in rows">
-                    <td>{{ index + 1 }}</td>
-                    <td>
-                        <select class="form-control" name="product_name" v-model="rows[index].id" @change="updateRow(index)">
-                            <option v-for="option in options" v-bind:value="option.id">{{ option.name}}</option>
-                        </select>
-                    </td>
-                    <td>
-                        <input type="input"
-                               class="form-control"
-                               placeholder="Quantity"
-                               name="quantity"
-                               @input="updateAmount(index)"
-                               v-model="rows[index].quantity" :disabled="rows[index].id === 0">
-                    </td>
-                    <td><input type="input" class="form-control" placeholder="Rate" name="rate" v-model="rows[index].rate" readonly></td>
-                    <td><input type="input" class="form-control" placeholder="Amount" name="Amount" v-model="rows[index].amount" readonly></td>
-                    <td>
-                        <a class="btn btn-primary" @click="addRow"><i class="fa fa-plus"></i></a>
-                        <a v-if="index != 0" class="btn btn-danger" @click="removeRow(index)"><i class="fa fa-minus"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="input" class="form-control" placeholder="Discount in %" v-model="discount" name="totalQuantity"  @input="calculateDiscount"></td>
-                    <td><input type="input" class="form-control" placeholder="Total Quantity" v-model="totalQuantity" name="totalQuantity" readonly><td>
-                    <td><input type="input" class="form-control" placeholder="Total Amount" v-model="totalAmount" name="totalAmount" readonly></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <table class="table">
+        <tbody>
+        <tr>
+            <td rowspan="9" style="border: 0;"></td>
+            <td colspan="3" rowspan="4" width="50%">
+                A.R Enterprice <br>
+                191 K Y Plaza(4th floor) <br>
+                Fokira Pool, Motijheel C/A <br>
+                Dhaka-1000 <br>
+                Phone:02-7191365 <br>
+                Mobile: 01716-511288, 01705-478851 <br>
+                Email: arenterprisebd2012@gmail.com, powerlube12@gmail.com
+
+            </td>
+            <td colspan="2">
+                Invoice No <br>
+                <b>
+                    2010
+                </b>
+            </td>
+            <td colspan="2">
+                Date <br>
+                <b>
+                    12-Mar-2021
+                </b>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                Delivery Note: <br>
+
+            </td>
+            <td colspan="2">Mode/Term of payment: <br>
+                <b>
+                    Cradit
+                </b>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                Aggent: <br>
+                <b>Md. Main Uddin</b>
+            </td>
+            <td colspan="2">Client <br></td>
+        </tr>
+        <tr>
+            <td colspan="2">Buyer's order No: <br></td>
+            <td colspan="2">Date <br></td>
+        </tr>
+        <tr>
+            <td colspan="3" rowspan="3">Buyer: <br></td>
+            <td colspan="2">Dispatch Document No: <br></td>
+            <td colspan="2">Date</td>
+        </tr>
+        <tr>
+            <td colspan="2">Address <br></td>
+            <td colspan="2">Destination <br></td>
+        </tr>
+        <tr>
+            <td colspan="4" rowspan="2">Terms of Delivery: <br></td>
+        </tr>
+        <tr>
+            <td colspan="3">28<br></td>
+        </tr>
+        <tr>
+            <td colspan="3" class="text-center">Description of Gods</td>
+            <td>Quantity/Cat</td>
+            <td>Rate/Cat</td>
+            <td colspan="2">Amount</td>
+        </tr>
+        <tr v-for="(row, index) in rows">
+            <td style="border: 0;">
+                <div class="btn-group">
+                    <a v-if="index === 0" href="#" class="btn btn-sm btn-info" @click.prevent="addRow"><i class="fa fa-plus"></i></a>
+                    <a v-if="index > 0" href="#" class="btn btn-sm btn-danger" @click.prevent="removeRow(index)"><i class="fa fa-trash"></i></a>
+                </div>
+            </td>
+            <td colspan="3" class="text-center">
+                <span class="font-italic" style="color: gray;" v-if="!row.editable" @click="row.editable = true">
+                    {{ row.name }}
+                </span>
+                <select name="product_name" v-else @blur="row.editable = false">
+                    <option v-for="option in options" value="{{ option.id }}">{{ option.name }}</option>
+                </select>
+
+            </td>
+            <td>{{ row.quantity }}</td>
+            <td>{{ row.rate }}</td>
+            <td colspan="2">{{ row.amount }}</td>
+        </tr>
+        <tr>
+            <td style="border: 0;"></td>
+            <td colspan="3" class="text-right">Total</td>
+            <td>{{ totalQuantity }}</td>
+            <td></td>
+            <td colspan="2">{{ totalAmount }}</td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
@@ -51,7 +105,7 @@
         name: "InvoiceTable",
         data() {
             return {
-                product: {id: 0, name: '', quantity: 0, rate: 0, amount: 0},
+                product: {id: 0, name: '--Select Product--', quantity: 0, rate: 0, amount: 0, editable: false},
                 rows: [],
                 options: [],
                 productName: '',
@@ -66,7 +120,7 @@
         },
         methods: {
             addRow: function () {
-                this.rows.push({id: 0, name: '', quantity: 0, rate: 0, amount: 0});
+                this.rows.push(this.product);
             },
 
             removeRow: function (index) {
@@ -124,5 +178,7 @@
 </script>
 
 <style scoped>
-
+    td {
+        border : 1px solid #dee2e6;
+    }
 </style>
